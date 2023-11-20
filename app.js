0c3be564d231d3333a6234197fa5f9a9fbca2033
chriskolb00 <cmlkolb@gmail.com>
2023-11-19 21:38:24 -0600

    Full pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, This is Christian'));
+app.get('/', (req, res) => res.send('Happy Monday, three days until Thanksgiving!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
